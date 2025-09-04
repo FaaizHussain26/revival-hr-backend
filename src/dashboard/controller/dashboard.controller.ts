00db966e3a6fb99081b60837fb51f0d90ab9c2fe
shaheer-ahmed-99 <shaheera@digitalauxilius.com>
2025-09-04 21:14:20 +0500

    Integrated authentication middleware on dashboard API routes

--- src/dashboard/controller/dashboard.controller.ts
+++ src/dashboard/controller/dashboard.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { DashboardService } from "../services/dashboard.service";
 import { ApiTags } from "@nestjs/swagger";
-
+import { JwtAuthGuard } from "src/common/guards/jwt-auth.guard";
 
 @Controller("dashboard")
 @ApiTags("Dashboard")
+@UseGuards(JwtAuthGuard)
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
